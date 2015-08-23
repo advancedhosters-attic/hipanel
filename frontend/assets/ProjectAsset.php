c173e9d53a6fbb135e309880022e1958c26391ca
Andrii Vasyliev <sol@hiqdev.com>
2015-08-23 19:19:53 +0000

    + ProjectAsset

---
+++ frontend/assets/ProjectAsset.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * AHnames HiPanel
+ *
+ * @link      https://ahnames.com/
+ * @package   hipanel
+ * @license   No license
+ * @copyright Copyright (c) 2015, AHnames (https://ahnames.com/)
+ */
+
+namespace project\frontend\assets;
+
+use yii\web\AssetBundle;
+
+class ProjectAsset extends AssetBundle
+{
+    public $sourcePath = '@project/frontend/assets/ProjectAssetFiles';
+
+    public $css = [
+        'css/project.css',
+    ];
+}
