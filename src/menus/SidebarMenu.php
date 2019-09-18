28b78b8ad7052572dd2e06a1de011bfdd9721e89
Maksym <smy980807@ukr.net>
2019-09-18 13:13:02 +0300

    Changed external services links to open in new tab (#10)
    * added link options to sidebar menu links
    
    * minor and added gitignore to tests

--- src/menus/SidebarMenu.php
+++ src/menus/SidebarMenu.php
@@ -10,6 +10,10 @@ class SidebarMenu extends Menu
     public function items()
     {
         $isVisible = true;
+        $targetBlank = [
+            'target'    => '_blank',
+            'rel'       => 'noopener noreferrer',
+        ];
 
         return [
             'cloud_servers' => [
@@ -17,30 +21,35 @@ class SidebarMenu extends Menu
                 'url' => 'https://websa.advancedhosting.com/servers',
                 'icon' => 'fa-cloud',
                 'visible' => $isVisible,
+                'linkOptions' => $targetBlank,
             ],
             'cloud_storage' => [
                 'label' => Yii::t('hipanel', 'Cloud Storage'),
                 'url' => 'https://websa.advancedhosting.com/bucketList',
                 'icon' => 'fa-cloud-upload',
                 'visible' => $isVisible,
+                'linkOptions' => $targetBlank,
             ],
             'video_cdn' => [
                 'label' => Yii::t('hipanel', 'Video CDN'),
                 'url' => 'https://vcp.advancedhosting.com/dashboard',
                 'icon' => 'fa-video-camera',
                 'visible' => $isVisible,
+                'linkOptions' => $targetBlank,
             ],
             'anycast_cdn' => [
                 'label' => Yii::t('hipanel', 'Anycast CDN'),
                 'url' => 'https://websa.advancedhosting.com/cdn',
                 'icon' => 'fa-globe',
                 'visible' => $isVisible,
+                'linkOptions' => $targetBlank,
             ],
             'anycast_dns' => [
                 'label' => Yii::t('hipanel', 'Anycast DNS'),
                 'url' => 'https://websa.advancedhosting.com/anycastns',
                 'icon' => 'fa-globe',
                 'visible' => $isVisible,
+                'linkOptions' => $targetBlank,
             ],
         ];
     }
